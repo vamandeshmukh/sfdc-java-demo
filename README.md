69a0a6d2afb8d7bc88924963e2f1493da3256247
vamandeshmukh <87980161+shridhar-javafsd@users.noreply.github.com>
2022-02-05 12:17:41 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# sfdc-java-demo
+ Core Java demo project for CG trainees SFDC 25 Jan 2022 batch 2
---
+++ src/UserInputDemo.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class UserInputDemo {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter num1");
+		int num1 = sc.nextInt();
+
+		System.out.println("Enter num2");
+		int num2 = sc.nextInt();
+
+		System.out.println(num1 + num2);
+
+	}
+
+}
