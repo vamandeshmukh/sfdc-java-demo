150963f900e861571f3266ca10e4e6bc620d89d2
vamandeshmukh <vamandeshmukh@gmail.com>
2022-02-07 15:43:25 +0530

    java code updated

--- src/com/capgemini/sfdc/oop/AbsDemo.java
+++ src/com/capgemini/sfdc/oop/AbsDemo.java
@@ -1,22 +1,36 @@
 package com.capgemini.sfdc.oop;
 
 // abstract - 
+// interface - enables multiple inheritance 
+
+interface SomeInterface {
+
+}
+
+interface StateGovt {
+
+}
+
+interface GovtOfIndia {
+
+	void paySalaries();
+
+}
 
 abstract class Rbi {
 
 	abstract void doKyc();
 
-//	void doKyc2() {
-//		// code - aadhaar, pan, voter
-//	}
+	void payInterest() {
 
+	}
 }
 
-class HdfcBank extends Rbi {
+class HdfcBank extends Rbi implements GovtOfIndia, StateGovt {
 
 	@Override
 	void doKyc() {
-		// specify the process 
+		// specify the process - aadhaar, pan, voter
 	}
 
 	void giveLoans() {
@@ -25,6 +39,12 @@ class HdfcBank extends Rbi {
 
 	void takeDeposits() {
 		// code
+	}
+
+	@Override
+	public void paySalaries() {
+		// TODO Auto-generated method stub
+
 	}
 
 }
