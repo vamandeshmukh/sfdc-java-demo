d8d8dce3bd6ba6d2531ed00dc6d07080b48a3f04
vamandeshmukh <vamandeshmukh@gmail.com>
2022-02-07 15:13:51 +0530

    java code updated

--- src/com/capgemini/sfdc/oop/EncapDemo.java
+++ src/com/capgemini/sfdc/oop/EncapDemo.java
@@ -2,6 +2,42 @@ package com.capgemini.sfdc.oop;
 
 // Encapsulation - bind data (fields / variables) and code (methods) together 
 // create private fields and create public methods (getters and setters) to access those fields 
+
+// Abstraction - minimum necessary representation - not related to the keyword "abstract" 
+// hide unnecessary details and show only the required functionality 
+
+class Employee {
+
+	private int employeeId;
+	private String firstName;
+	private double salary;
+
+	public int getEmployeeId() {
+		return employeeId;
+	}
+
+	public void setEmployeeId(int employeeId) {
+		this.employeeId = employeeId;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
+}
+
 class BankCustomer {
 
 	private double balance;
@@ -34,6 +70,8 @@ public class EncapDemo {
 		customer1.withdraw(600);
 		System.out.println(customer1.getBalance());
 //		customer1.balance = 1500; // not allowed
+		System.out.println(customer1.getBalance());
+		customer1.setBalance(2000);
 		System.out.println(customer1.getBalance());
 	}
 }
---
+++ src/com/capgemini/sfdc/oop/InheritDemo.java
@@ -0,0 +1,22 @@
+package com.capgemini.sfdc.oop;
+
+//inheritance - a subclass (child class) accesses members of superclass (parent class)
+//acquire properties from parents 
+
+class Parent {
+	int num;
+}
+
+class Child extends Parent {
+	int num2;
+}
+
+public class InheritDemo {
+	public static void main(String[] args) {
+		Child ch = new Child();
+		ch.num2 = 20;
+		System.out.println(ch.num2);
+		ch.num = 10;
+		System.out.println(ch.num);
+	}
+}
