88351d1768aa96ccfa1d876e29fbe328dcb7a157
vamandeshmukh <vamandeshmukh@gmail.com>
2022-02-08 12:56:34 +0530

    commited the code

--- src/com/capgemini/sfdc/str/WrapperDemo.java
+++ src/com/capgemini/sfdc/str/WrapperDemo.java
@@ -1,5 +1,11 @@
 package com.capgemini.sfdc.str;
 
+// wrapper classes 
+// int - Integer
+// byte - Byte
+// char - Character 
+
+
 public class WrapperDemo {
 
 	public static void main(String[] args) {
@@ -8,8 +14,19 @@ public class WrapperDemo {
 ////		str.
 //		int num = 10;
 ////		num.
-		
-		Integer num = 10;
+
+//		Integer num = 10;
+//		System.out.println(num.intValue());
+
+		String str = "100";
+		String str2 = "200";
+		String str3;
+
+		int num1 = Integer.parseInt(str);
+		int num2 = Integer.parseInt(str2);
+		int num3 = num1 + num2;
+		str3 = Integer.toString(num3);
+		System.out.println(str3);
 
 	}
 
