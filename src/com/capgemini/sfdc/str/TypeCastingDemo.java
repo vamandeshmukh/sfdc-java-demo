6f25b3cd2d9954ff2745b57d5e2ac63fe613a6c6
vamandeshmukh <vamandeshmukh@gmail.com>
2022-02-08 12:14:21 +0530

    commited the code

--- src/com/capgemini/sfdc/str/TypeCastingDemo.java
+++ src/com/capgemini/sfdc/str/TypeCastingDemo.java
@@ -4,11 +4,24 @@ public class TypeCastingDemo {
 
 	public static void main(String[] args) {
 
-		byte b1 = 10, b2 = 20, b3 = b1 + b2;
+//		byte short int long char 
+//		byte short int char 
+
+		byte b1 = 10, b2 = 20, b3 = (byte) (b1 + b2);
 
 		System.out.println(b3);
 
 		byte b4 = -128; // -128 to 127
+		
+		System.out.println("byte range- ");
+		System.out.println(Byte.MIN_VALUE + " to " + Byte.MAX_VALUE);
+		System.out.println("short range- ");
+		System.out.println(Short.MIN_VALUE + " to " + Short.MAX_VALUE);
+		System.out.println("int range- ");
+		System.out.println(Integer.MIN_VALUE + " to " + Integer.MAX_VALUE);
+		System.out.println("long range- ");
+		System.out.println(Long.MIN_VALUE + " to " + Long.MAX_VALUE);
+		
 
 	}
 
